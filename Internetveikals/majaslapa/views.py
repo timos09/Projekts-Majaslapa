9d1b2574f8730af9d16fc1ca40270c18688837ce
EmilsBlinds <wineek23gt@gmail.com>
2022-10-01 20:38:53 +0300

    register backend

--- Internetveikals/majaslapa/views.py
+++ Internetveikals/majaslapa/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import  CreateUserForm
+# from django.conf import settings
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your views here.
+# from .models import *
+# from .forms import OrderForm, CreateUserForm
+# from .filters import OrderFilter
+
 def sakums(request):
     return render(request, 'majaslapa/home.html')
 
@@ -17,7 +26,17 @@ def login(request):
     return render(request, 'majaslapa/login.html')
 
 def register(request):
-    return render(request, 'majaslapa/register.html')
+    form = CreateUserForm(request.POST)
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+
+    context = {'form': form}
+    return render(request, 'majaslapa/register.html', context)
 
 def search(request):
     return render(request, 'majaslapa/search.html')
